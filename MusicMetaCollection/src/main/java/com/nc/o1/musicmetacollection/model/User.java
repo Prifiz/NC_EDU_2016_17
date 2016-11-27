641debd9eba48f392c80eaa8214e401e93a13044
tr1ben <tr1ben@mail.ru>
2016-11-27 16:01:09 +0400

    Added English Javadoc for User

--- MusicMetaCollection/src/main/java/com/nc/o1/musicmetacollection/model/User.java
+++ MusicMetaCollection/src/main/java/com/nc/o1/musicmetacollection/model/User.java
@@ -1,29 +1,18 @@
 package com.nc.o1.musicmetacollection.model;
 
-/**
- * Describes User entity
- *
- * @author SteVL
- */
 public class User {
 
-    /**
-     * How to call user
-     */
     private String username;
-    /**
-     * login & id
-     */
     private String email;
     private String password;
 
     /**
-     * Creates new user
+     * Constructs new User based on the provided username, email address and
+     * password.
      *
-     * @param username
-     * @param email
-     * @param password
-     * @param group
+     * @param username name
+     * @param email email address
+     * @param password password
      */
     public User(String username, String email, String password) {
         this.username = username;
@@ -32,34 +21,64 @@ public class User {
     }
 
     /**
-     * Creates existing user
+     * Constructs new User based on the provided email address.
      *
-     * @param email
+     * @param email email address
      */
     public User(String email) {
         this.email = email;
     }
 
+    /**
+     * Returns the username.
+     *
+     * @return an username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Returns the email address.
+     *
+     * @return an email address
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Returns the password.
+     *
+     * @return a password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Replaces the username with the specified username.
+     *
+     * @param username new username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Replaces the email address with the specified email address.
+     *
+     * @param email new email address
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Replaces the password with the specified password.
+     *
+     * @param password new password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
@@ -81,6 +100,11 @@ public class User {
         System.out.println("deleteUser isn't supported yet.");
     }
 
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return a string representation of the object
+     */
     @Override
     public String toString() {
         StringBuilder sbl = new StringBuilder();
@@ -88,19 +112,25 @@ public class User {
         return sbl.toString();
     }
 
-    //need to test
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     *
+     * @param obj the reference object with which to compare.
+     * @return true if this object is the same as the obj argument; false
+     * otherwise.
+     */
     @Override
-    public boolean equals(Object object) {
-        if (this == object) {
+    public boolean equals(Object obj) {
+        if (this == obj) {
             return true;
         }
-        if (object == null) {
+        if (obj == null) {
             return false;
         }
-        if (getClass() != object.getClass()) {
+        if (getClass() != obj.getClass()) {
             return false;
         }
-        User other = (User) object;
+        User other = (User) obj;
         if (!this.username.equalsIgnoreCase(other.username)) {
             return false;
         }
@@ -111,6 +141,11 @@ public class User {
         return true;
     }
 
+    /**
+     * Returns a hash code value for the object.
+     *
+     * @return a hash code value for this object
+     */
     @Override
     public int hashCode() {
         int hash = 37;
