42d69cb820dc88b23635f65939da4150a0437360
tr1ben <tr1ben@mail.ru>
2017-01-23 18:09:00 +0400

    Renamed interface elements. Updated method "initComponents()".

--- MusicMetaCollection/app/src/main/java/com/nc/o1/musicmetacollection/view/MainFrame.java
+++ MusicMetaCollection/app/src/main/java/com/nc/o1/musicmetacollection/view/MainFrame.java
@@ -43,18 +43,37 @@ import javax.swing.filechooser.FileFilter;
 
 public class MainFrame extends JFrame {
 
-    private final JMenuBar topMenu;
+    private JMenuBar topMenu;
 
-    private javax.swing.JComboBox<String> searchTrackParam;
-    private JRadioButton searchBt, regexpBt;
-    private final JMenu fileMenu, fileSaveMenu, fileLoadMenu;
+    private JComboBox<String> searchTrackParam;
+    private JRadioButton substringSearchButton;
+    private JRadioButton regexpSearchButton;
+    private JMenu fileMenu;
+    private JMenu fileSaveMenu;
+    private JMenu fileLoadMenu;
     private JFileChooser fileChooser;
-    private final JMenuItem fileSaveXML, fileSaveSrlz;
-    private final JMenuItem fileLoadXML, fileLoadSrlz;
-    private final JMenuItem helpMenu;
-    private final JMenuItem fileExit;
+    Color backgroundColor;
+    TrackListTableModel trackListTableModel;
+    JScrollPane jscrlp;
+    JPanel searchPanel;
+    JPanel searchTypePanel;
+    JPanel btnPnl;
+    JPanel bottombtnPnl;
+    JButton showTrackListButton;
+    JButton addTrackButton;
+    JButton removeTrackButton;
+    JButton searchTracksButton;
+    JButton clearTableButton;
+    ButtonGroup searchRadioButtonsGroup;
+    private JMenuItem fileSaveXML;
+    private JMenuItem fileSaveSerializable;
+    private JMenuItem fileLoadXML;
+    private JMenuItem fileLoadSerializable;
+    private JMenuItem helpMenu;
+    private JMenuItem fileExit;
 
-    private TrackList trackList, cleanList;
+    private TrackList trackList;
+    private TrackList cleanList;
     private Vector searchParamsList;
     private JTable table;
     private JFrame frame;
@@ -64,7 +83,17 @@ public class MainFrame extends JFrame {
      * Creates Main Window.
      */
     public MainFrame() {
-        this.initComponents();
+        initComponents();
+    }
+
+    private void initComponents() {
+        frame = new JFrame();
+        trackList = new TrackList();
+        cleanList = new TrackList();
+        fileChooser = new JFileChooser();
+        searchParamsList = this.getSearchParamsList();
+        fileChooser = new JFileChooser();
+
         //<editor-fold defaultstate="collapsed" desc="topMenu">
         topMenu = new JMenuBar();
         frame.setJMenuBar(topMenu);
@@ -81,9 +110,9 @@ public class MainFrame extends JFrame {
         fileLoadXML = new JMenuItem("XML file...");
         fileLoadMenu.add(fileLoadXML);
 
-        fileLoadSrlz = new JMenuItem("Serialized file...");
-        fileLoadMenu.add(fileLoadSrlz);
-        fileLoadSrlz.addActionListener(new ActionListener() {
+        fileLoadSerializable = new JMenuItem("Serialized file...");
+        fileLoadMenu.add(fileLoadSerializable);
+        fileLoadSerializable.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 fileLoadSrlzActiionPerformed(e);
@@ -98,14 +127,14 @@ public class MainFrame extends JFrame {
         fileSaveXML = new JMenuItem("XML file...");
         fileSaveMenu.add(fileSaveXML);
 
-        fileSaveSrlz = new JMenuItem("Serialized file...");
-        fileSaveSrlz.addActionListener(new java.awt.event.ActionListener() {
+        fileSaveSerializable = new JMenuItem("Serialized file...");
+        fileSaveSerializable.addActionListener(new java.awt.event.ActionListener() {
             @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 serializeActionPerformed(evt);
             }
         });
-        fileSaveMenu.add(fileSaveSrlz);
+        fileSaveMenu.add(fileSaveSerializable);
 
         helpMenu = new JMenuItem("Help");
         helpMenu.addActionListener(new java.awt.event.ActionListener() {
@@ -128,91 +157,90 @@ public class MainFrame extends JFrame {
         //</editor-fold>
         frame.setLayout(new BorderLayout());
 
-        Color color = frame.getBackground();
+        backgroundColor = frame.getBackground();
         //Создаем новую таблицу на основе двумерного массива данных и заголовков
-        TrackListTableModel tltm = new TrackListTableModel(trackList);
-        table = new JTable(tltm);
-        //Создаем панель прокрутки и включаем в ее состав нашу таблицу
-        JScrollPane jscrlp = new JScrollPane(table);
+        trackListTableModel = new TrackListTableModel(trackList);
+        table = new JTable(trackListTableModel);
+        //Создаем панель прокрутки и включаем её в состав нашей таблицы
+        jscrlp = new JScrollPane(table);
         //Устаналиваем размеры прокручиваемой области
         table.setPreferredScrollableViewportSize(new Dimension(650, 600));
         //Добавляем в контейнер нашу панель прокрути и таблицу вместе с ней
         frame.getContentPane().add(jscrlp);
         //Отображаем контейнер
         frame.setVisible(true);
-        table.setBackground(color);
-        JPanel searchPanel = new JPanel(new FlowLayout());
-        JPanel radioPanel = new JPanel(new GridBagLayout());
-        JPanel btnPnl = new JPanel(new BorderLayout());
-        JPanel bottombtnPnl = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        
+        table.setBackground(backgroundColor);
+        searchPanel = new JPanel(new FlowLayout());
+        searchTypePanel = new JPanel(new GridBagLayout());
+        btnPnl = new JPanel(new BorderLayout());
+        bottombtnPnl = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
         searchTrackParam = new JComboBox(searchParamsList);
 
-        JButton show = new JButton("Show All");
-        show.addActionListener(new java.awt.event.ActionListener() {
+        showTrackListButton = new JButton("Show All");
+        showTrackListButton.addActionListener(new java.awt.event.ActionListener() {
             @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 showBtActionPerformed(evt);
             }
         });
-        JButton add = new JButton("Add Track");
-        add.addActionListener(new java.awt.event.ActionListener() {
-
+        
+        addTrackButton = new JButton("Add Track");
+        addTrackButton.addActionListener(new java.awt.event.ActionListener() {
             @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 addBtActionPerformed(evt);
             }
         });
 
-        JButton del = new JButton("Delete Track");
-        del.addActionListener(new java.awt.event.ActionListener() {
-
+        removeTrackButton = new JButton("Remove Track");
+        removeTrackButton.addActionListener(new java.awt.event.ActionListener() {
             @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 deleteBtActionPerformed(evt);
             }
         });
 
-        JButton search = new JButton("Search Track");
-        search.addActionListener(new java.awt.event.ActionListener() {
+        searchTracksButton = new JButton("Search Track");
+        searchTracksButton.addActionListener(new java.awt.event.ActionListener() {
             @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 searchBtActionPerformed(evt);
             }
         });
 
-        JButton clear = new JButton("Clear Table");
-        clear.addActionListener(new java.awt.event.ActionListener() {
+        clearTableButton = new JButton("Clear Table");
+        clearTableButton.addActionListener(new java.awt.event.ActionListener() {
             @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 clearBtActionPerformed(evt);
             }
         });
 
-        ButtonGroup group = new ButtonGroup();
-        searchBt = new JRadioButton("substring", true);
-        regexpBt = new JRadioButton("regexp", false);
-        group.add(searchBt);
-        group.add(regexpBt);
+        searchRadioButtonsGroup = new ButtonGroup();
+        substringSearchButton = new JRadioButton("substring", true);
+        regexpSearchButton = new JRadioButton("regexp", false);
+        searchRadioButtonsGroup.add(substringSearchButton);
+        searchRadioButtonsGroup.add(regexpSearchButton);
         searchText = new JTextField(20);
-        show.setBackground(Color.white);
-        add.setBackground(Color.white);
-        del.setBackground(Color.white);
-        search.setBackground(Color.white);
-        clear.setBackground(Color.white);
+        showTrackListButton.setBackground(Color.white);
+        addTrackButton.setBackground(Color.white);
+        removeTrackButton.setBackground(Color.white);
+        searchTracksButton.setBackground(Color.white);
+        clearTableButton.setBackground(Color.white);
         searchPanel.add(new JLabel("Search: "));
         searchPanel.add(searchTrackParam);
         searchPanel.add(searchText);
-        searchPanel.add(search);
-        radioPanel.add(searchBt);
-        radioPanel.add(regexpBt);
-        bottombtnPnl.add(clear);
-        bottombtnPnl.add(show);
-        bottombtnPnl.add(add);
-        bottombtnPnl.add(del);
+        searchPanel.add(searchTracksButton);
+        searchTypePanel.add(substringSearchButton);
+        searchTypePanel.add(regexpSearchButton);
+        bottombtnPnl.add(clearTableButton);
+        bottombtnPnl.add(showTrackListButton);
+        bottombtnPnl.add(addTrackButton);
+        bottombtnPnl.add(removeTrackButton);
 
         btnPnl.add(searchPanel, BorderLayout.NORTH);
-        btnPnl.add(radioPanel, BorderLayout.CENTER);
+        btnPnl.add(searchTypePanel, BorderLayout.CENTER);
         btnPnl.add(bottombtnPnl, BorderLayout.SOUTH);
 
         table.getTableHeader().setReorderingAllowed(false);
@@ -240,14 +268,6 @@ public class MainFrame extends JFrame {
         });
     }
 
-    private void initComponents() {
-        frame = new JFrame();
-        trackList = new TrackList();
-        cleanList = new TrackList();
-        fileChooser = new JFileChooser();
-        searchParamsList = this.getSearchParamsList();
-    }
-
     private Vector getSearchParamsList() {
         Vector<String> searchParamsList = new Vector<>();
         searchParamsList.add("Artist");
@@ -266,13 +286,8 @@ public class MainFrame extends JFrame {
         System.exit(0);
     }
 
-    private void serializeToFile() {
-
-    }
-
     private void serializeActionPerformed(java.awt.event.ActionEvent evt) {
-        JFileChooser chooser = new JFileChooser();
-        chooser.setFileFilter(new FileFilter() {
+        fileChooser.setFileFilter(new FileFilter() {
             @Override
             public boolean accept(File file) {
                 return file.isDirectory() || file.getAbsolutePath().endsWith(".bin");
@@ -283,10 +298,10 @@ public class MainFrame extends JFrame {
                 return "Binary files (*.bin)";
             }
         });
-        int ret = chooser.showSaveDialog(topMenu);
+        int ret = fileChooser.showSaveDialog(topMenu);
         if (ret == JFileChooser.APPROVE_OPTION) {
             FileSrlzController fileSrlzCntrl = new FileSrlzController(trackList);
-            File file = chooser.getSelectedFile().getAbsoluteFile();
+            File file = fileChooser.getSelectedFile().getAbsoluteFile();
             String path = file.getPath();
             if (!path.endsWith(".bin")) {
                 file = new File(path + ".bin");
@@ -328,11 +343,8 @@ public class MainFrame extends JFrame {
     }
 
     private void showTracks(TrackList trackList) {
-        //создаем модель на основе полученного треклиста
         TrackListTableModel model = new TrackListTableModel(trackList);
-        // и применяем ее к таблице
         table.setModel(model);
-        //делаем первую строку выделенной
         if (model.getRowCount() > 0) {
             table.setRowSelectionInterval(0, 0);
         }
@@ -365,7 +377,7 @@ public class MainFrame extends JFrame {
         SearchTrackController sCtrl = new SearchTrackController(trackList);
         String searchParam = searchTrackParam.getModel().getSelectedItem().toString();
         TrackList schTracks;
-        if (searchBt.isSelected()) {
+        if (substringSearchButton.isSelected()) {
             schTracks = sCtrl.substringSearch(searchParam, searchText.getText());
         } else {
             schTracks = sCtrl.regexpSearch(searchParam, searchText.getText());
